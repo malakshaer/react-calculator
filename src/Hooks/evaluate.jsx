3fb689cc0992867d463fdbbe0f20f8bb4b17c0e2
malakshaer <shaermk06@gmail.com>
2022-10-23 10:40:35 +0300

    evaluate function added

---
+++ src/Hooks/evaluate.jsx
@@ -0,0 +1,25 @@
+const evaluate = ({ currentOperand, previousOperand, operation }) => {
+  const prev = parseFloat(previousOperand);
+  const current = parseFloat(currentOperand);
+  if (isNaN(prev) || isNaN(current)) return "";
+  let computation = "";
+  switch (operation) {
+    case "+":
+      computation = prev + current;
+      break;
+    case "-":
+      computation = prev - current;
+      break;
+    case "*":
+      computation = prev * current;
+      break;
+    case "÷":
+      computation = prev / current;
+      break;
+    default:
+      return;
+  }
+
+  return computation.toString();
+};
+export default evaluate;
